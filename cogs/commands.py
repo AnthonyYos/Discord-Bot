6e6db05adb392177b02f0051e4d4be885d0c7dc0
Anthony Yos <anthonyvyos@gmail.com>
2021-10-18 20:06:32 -0500

    Add Try Except to roll command Catches negative numbers

--- cogs/commands.py
+++ cogs/commands.py
@@ -15,11 +15,21 @@ class LearningDiscordBot_commands(commands.Cog, name = "User Commands"):
     # Roll Dice command
     @commands.command(help = "Rolls x-amount of y-sided dice")
     async def roll(self, ctx, number_of_dice: int, number_of_sides: int):
+        try:
+            if number_of_dice <= 0 or number_of_sides <= 0:
+                raise ValueError
+            # if isinstance(number_of_dice,int) != True or isinstance(number_of_sides,int) != True:
+            #     raise TypeError
         # REMINDER this works because of list comprehension
-        dice = [
+            dice = [
                 str(random.randint(1, number_of_sides))
                 for dice in range(number_of_dice)
-            ]          
+            ]
+        except (ValueError):
+            await ctx.send("Input value(s) must be a value of 1 or greater")
+        # except (TypeError):
+        #     await ctx.send("Input value(s) must be a/an integer(s)")
+            
         await ctx.send(", ".join(dice))
 
     # 8ball command
